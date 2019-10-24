31c8273810c8885eb6833eb98887dd45760cc14a
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-24 19:18:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,14 +8,14 @@
  puts "Hi, my name is #{name}."
 #end
 
-def introduction_with_language(name,language)
-  p "Hi my name is #{name} and im learning to program in #{language}."
-end
-
-
-#def introduction_with_language_optional(name,language = "Ruby")
+#def introduction_with_language(name,language)
   p "Hi my name is #{name} and im learning to program in #{language}."
 #end
+
+
+def introduction_with_language_optional(name,language = "Ruby")
+  p "Hi my name is #{name} and im learning to program in #{language}."
+end
 
 
 
